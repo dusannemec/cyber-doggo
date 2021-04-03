d2c0d715b6f35fcccec9525fbbbdc6e581dcad84
Reihtw <reihtw@pop-os.localdomain>
2021-04-03 13:52:35 -0300

    First commit. Mk1 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'cyber-doggo';
+
+  plainText: string;
+  base64: string;
+
+  encode(plainText: string) {
+    this.plainText = plainText;
+    this.base64 = btoa(plainText);
+  }
+
+  decode(base64: string) {
+    this.base64 = base64;
+    this.plainText = atob(base64);
+  }
 }
